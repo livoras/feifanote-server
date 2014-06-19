6cae199a7221477d96b9b8c8989939194453dbbd
livoras <livoras@163.com>
2014-06-19 19:49:14 +0800

    fix config DATABASE_URI doesnt work

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
 DEBUG = True
-DATABASE_URI = "sqlite:///"
+
+if sys.argv[0] == "app.py":
+    DATABASE_URI = "sqlite:///test.db"
+else:
+    DATABASE_URI = "sqlite:///:memory:"
 ECHO = False
 SECRET_KEY = '9687d209a9ff0713da8edae47dce398'
--- test/__init__.py
+++ test/__init__.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
 import config
-
-config.DATABASE_URI = "sqlite:///:memory:"
-config.ECHO = False
-
 from common import db
 
 def http(c, method, url, data={}):
