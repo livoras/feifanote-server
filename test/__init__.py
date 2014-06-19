3cebce393afde48868c07384f8d1946ad4bdfd4f
livoras <livoras@163.com>
2014-06-19 19:49:43 +0800

    ajust indention

--- test/__init__.py
+++ test/__init__.py
@@ -4,7 +4,7 @@ import config
 from common import db
 
 def http(c, method, url, data={}):
-  send_fn = getattr(c, method)
-  json_data = json.dumps(data)
-  headers = {'Content-Type': 'application/json'}
-  return send_fn(url, headers=headers, data=json_data)
+    send_fn = getattr(c, method)
+    json_data = json.dumps(data)
+    headers = {'Content-Type': 'application/json'}
+    return send_fn(url, headers=headers, data=json_data)
