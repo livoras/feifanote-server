319907f8e107757470160cb3dc0adba14357bed1
livoras <livoras@163.com>
2014-06-26 13:14:11 +0800

    add feedbacks show

--- apis/feedback.py
+++ apis/feedback.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
-from flask import Blueprint, jsonify, request, session
+from flask import Blueprint, jsonify, request, session, make_response
 from common.utils import message
 from businesses import feedback
 from ._helpers import require_login
@@ -17,3 +17,12 @@ def create_feedback():
     data["user_id"] = session["id"]
     feedback.create_feedback(data)
     return message("OK.", 201)
+
+@api.route("/feedbacks/<int:start>/<int:end>", methods=["GET"])
+def get_feedbacks(start, end):
+  feedbacks = feedback.get_feedbacks(start, end)
+  result = "<ul>"
+  for fb in feedbacks:
+    result = result + "<li><b>%s</b> said: <i>%s</i></li><hr>" % (fb.user.email, fb.content)
+  result = result + "</ul>"
+  return result
--- businesses/feedback.py
+++ businesses/feedback.py
@@ -5,3 +5,7 @@ from common.db import session
 def create_feedback(feedback_data):
     session.add(Feedback(**feedback_data))
     session.commit()
+
+def get_feedbacks(start, end):
+    feedbacks = session.query(Feedback).all()
+    return feedbacks[start:end]
